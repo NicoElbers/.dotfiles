a4f361e82da53caaf0ec94113afe46856e35334b
Nico Elbers <nico.b.elbers@gmail.com>
2024-07-03 17:31:06 +0200

    remove some unused imports

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -1,9 +1,9 @@
-{ lib, config, ...}:
+{ pkgs, ...}:
 {
   config = {
     environment.pathsToLink = [ "/share/zsh" ];
 
-    # Prevent new user dialog
+# Prevent new user dialog
     system.userActivationScripts.zshrc = "touch .zshrc";
 
     programs.zsh = {
@@ -11,7 +11,7 @@
       enableCompletion = true;
 
       shellInit = ''
-      '';
+        '';
       shellAliases = {
         rebuild = "sudo nixos-rebuild switch --impure --flake ~/.dotfiles#omen";
       };
@@ -21,7 +21,18 @@
         plugins = [];
         theme = "agnoster";
       };
+      plugins = [
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.8.0";
+            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+          };
+        }
+      ];
     };
-
   };
 }
