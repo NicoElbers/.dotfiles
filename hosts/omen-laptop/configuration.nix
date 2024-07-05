51872697e6f6047d75dfaaae13e736f9265d0d4d
Nico Elbers <nico.b.elbers@gmail.com>
2024-07-05 15:00:17 +0200

    Actually use SDDM

--- hosts/omen-laptop/configuration.nix
+++ hosts/omen-laptop/configuration.nix
@@ -69,7 +69,7 @@ in
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  # services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
