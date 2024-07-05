2f420275f514c21db5ff4eebffbf3ddc13a75bee
Nico Elbers <nico.b.elbers@gmail.com>
2024-07-06 01:50:50 +0200

    set bluetooth

--- modules/base.nix
+++ modules/base.nix
@@ -1,13 +1,19 @@
 {config, pkgs, ...}:
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  environment.systemPackages = with pkgs; [
-    vim 
-    wget
-    kitty   
-    git
-    wl-clipboard
-    polkit
+  imports = [
+    ./bluetooth.nix
   ];
+
+  config = {
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+    environment.systemPackages = with pkgs; [
+        vim 
+        wget
+        kitty   
+        git
+        wl-clipboard
+        polkit
+    ];
+  }; 
 }
