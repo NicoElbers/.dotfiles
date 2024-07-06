1fbf0f75d437d2619073dca080f21f12d65ccfca
Nico Elbers <nico.b.elbers@gmail.com>
2024-07-06 05:18:49 +0200

    can't be fucked to remove this rn

--- hosts/omen-laptop/configuration.nix
+++ hosts/omen-laptop/configuration.nix
@@ -45,8 +45,6 @@ in
   networking.hostName = "omen"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -122,6 +120,9 @@ in
   # $ nix search wget
   # FIXME: Move into base
   environment.systemPackages = with pkgs; [
+    # # Get NM to work?
+    # networkmanagerapplet
+
     vim 
     wget
     kitty   
