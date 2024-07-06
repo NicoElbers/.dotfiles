d8d11292206502843fe30c283023643bbe72353f
Nico Elbers <nico.b.elbers@gmail.com>
2024-07-06 04:54:44 +0200

    get base compiling again

--- modules/base.nix
+++ modules/base.nix
@@ -15,19 +15,20 @@
         wl-clipboard
         polkit
     ];
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    # Set browser
+    programs.firefox.enable = true;
+    xdg.mime.defaultApplications = {
+      "text/html"                 = "firefox.desktop";
+      "x-scheme-handler/http"     = "firefox.desktop";
+      "x-scheme-handler/https"    = "firefox.desktop";
+      "x-scheme-handler/about"    = "firefox.desktop";
+      "x-scheme-handler/unknown"  = "firefox.desktop";
+    };
+    environment.sessionVariables.DEFAULT_BROWSER = "${pkgs.firefox}/bin/firefox";
   }; 
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # Set browser
-  programs.firefox.enable = true;
-  xdg.mime.defaultApplications = {
-    "text/html"                 = "firefox.desktop";
-    "x-scheme-handler/http"     = "firefox.desktop";
-    "x-scheme-handler/https"    = "firefox.desktop";
-    "x-scheme-handler/about"    = "firefox.desktop";
-    "x-scheme-handler/unknown"  = "firefox.desktop";
-  };
-  environment.sessionVariables.DEFAULT_BROWSER = "${pkgs.firefox}/bin/firefox";
 }
