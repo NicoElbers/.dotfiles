001732091be5e9e7a4612a23eabed6e2664e1716
Nico Elbers <nico.b.elbers@gmail.com>
2024-07-14 02:08:03 +0200

    Update tracked keywords for st

--- modules/nixCats/config/lua/plugins/todo.lua
+++ modules/nixCats/config/lua/plugins/todo.lua
@@ -8,7 +8,7 @@ return {
     keys = {
         { "]t",         function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
         { "[t",         function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
-        { "<leader>st", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>",    desc = "Todo/Fix/Fixme" },
+        { "<leader>st", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME,TEST,TESTS<cr>",    desc = "Keywords I wanna track" },
     },
     opts = {},
 }
