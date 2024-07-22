dccae41ddb5ac558037b9b9848ed88c6436fb329
Nico Elbers <nico.b.elbers@gmail.com>
2024-07-22 02:13:57 +0200

    update zig template to have zls

--- templates/zig/flake.nix
+++ templates/zig/flake.nix
@@ -3,18 +3,30 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.zig.url = "github:mitchellh/zig-overlay";
 
-  outputs = { nixpkgs, flake-utils, zig, ... }:
+  inputs.zig = {
+    url = "github:mitchellh/zig-overlay";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  inputs.zls = {
+    url = "github:zigtools/zls";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, flake-utils, zls, zig, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        zls_bound = zls.packages.${system}.zls;
+        zig_bound = zig.packages.${system}.master;
       in
       {
         devShells.default = with pkgs; mkShell {
           packages = [
             bashInteractive
-            zig.packages.${system}.master
+            zig_bound
+            zls_bound
           ];
         };
       });
