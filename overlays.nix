f64a55deced4efd8c983addf00e3d236fe13b61e
Nico Elbers <nico.b.elbers@gmail.com>
2024-11-06 00:45:27 +0100

    resolve 2ca4d42d6213

--- overlays.nix
+++ overlays.nix
@@ -3,17 +3,5 @@
   # additions = final: _prev: import ./path/to/pkgs final.pkgs;
 
   modifications = final: prev: {
-    tlp = prev.tlp.overrideAttrs (old: {
-      makeFlags = (old.makeFlags or [ ]) ++ [
-        "TLP_ULIB=/lib/udev"
-        "TLP_NMDSP=/lib/NetworkManager/dispatcher.d"
-        "TLP_SYSD=/lib/systemd/system"
-        "TLP_SDSL=/lib/systemd/system-sleep"
-        "TLP_ELOD=/lib/elogind/system-sleep"
-        "TLP_CONFDPR=/share/tlp/deprecated.conf"
-        "TLP_FISHCPL=/share/fish/vendor_completions.d"
-        "TLP_ZSHCPL=/share/zsh/site-functions"
-      ];
-    });
   };
 }
