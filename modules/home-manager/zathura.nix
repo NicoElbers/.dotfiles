e5f7aef64acd181bc990eccb063a928c5f2fad2a
Nico Elbers <nico.b.elbers@gmail.com>
2025-03-15 16:20:15 +0100

    zathura theme

--- modules/home-manager/zathura.nix
+++ modules/home-manager/zathura.nix
@@ -1,8 +1,30 @@
+{pkgs, ...}:
 {
-  config = {
-    programs.zathura = {
-      enable = true;
+  programs.zathura = {
+    enable = true;
+    options = {
+      # recolor-lightcolor = "rgba(0,0,0,0)";
+      # default-bg = "rgba(0,0,0,0.7)";
 
+      # font = "Inter 12";
+      selection-notification = true;
+
+      selection-clipboard = "clipboard";
+      adjust-open = "best-fit";
+      # pages-per-row = "1";
+      # scroll-page-aware = "true";
+      # scroll-full-overlap = "0.01";
+      # scroll-step = "100";
+      # zoom-min = "10";
+    };
+
+    extraConfig = "include catppuccin-mocha";
+  };
+
+  xdg.configFile = {
+    "zathura/catppuccin-mocha".source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/catppuccin/zathura/0adc53028d81bf047461bc61c43a484d11b15220/src/catppuccin-mocha";
+      hash = "sha256-POxMpm77Pd0qywy/jYzZBXF/uAKHSQ0hwtXD4wl8S2Q=";
     };
   };
 }
