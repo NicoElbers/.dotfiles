2d807eb05d23732fd22de1cd121a83ea8a15d409
Nico Elbers <nico.b.elbers@gmail.com>
2025-08-06 21:40:01 +0200

    update kernel fix

--- overlays.nix
+++ overlays.nix
@@ -2,10 +2,17 @@
 [
   # https://github.com/NixOS/nixpkgs/issues/429888
   (final: prev: {
-    linux-manual = prev.linux-manual.overrideAttrs (old: {
-      installCheckPhase = ''
-        echo "Skipping kmalloc(9) check for kernel 6.16"
-      '';
-    });
-  })
+     linux-manual = prev.linux-manual.overrideAttrs (old: {
+       nativeBuildInputs = [
+         prev.perl
+         prev.python3
+       ];
+       postPatch = ''
+         chmod +x scripts/kernel-doc.py scripts/split-man.pl
+         patchShebangs --build \
+           scripts/kernel-doc.py \
+           scripts/split-man.pl
+         '';
+     });
+   })
 ]
