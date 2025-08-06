18576b01f19f221b5f200b73380063341c0b345c
Nico Elbers <nico.b.elbers@gmail.com>
2025-08-06 21:56:33 +0200

    get shit working :'(

--- overlays.nix
+++ overlays.nix
@@ -1,18 +1,18 @@
 { inputs, ... }: 
 [
   # https://github.com/NixOS/nixpkgs/issues/429888
-  (final: prev: {
-     linux-manual = prev.linux-manual.overrideAttrs (old: {
-       nativeBuildInputs = [
-         prev.perl
-         prev.python3
-       ];
-       postPatch = ''
-         chmod +x scripts/kernel-doc.py scripts/split-man.pl
-         patchShebangs --build \
-           scripts/kernel-doc.py \
-           scripts/split-man.pl
-         '';
-     });
-   })
+  # (final: prev: {
+  #    linux-manual = prev.linux-manual.overrideAttrs (old: {
+  #      nativeBuildInputs = [
+  #        prev.perl
+  #        prev.python3
+  #      ];
+  #      postPatch = ''
+  #        chmod +x scripts/kernel-doc.py scripts/split-man.pl
+  #        patchShebangs --build \
+  #          scripts/kernel-doc.py \
+  #          scripts/split-man.pl
+  #        '';
+  #    });
+  #  })
 ]
