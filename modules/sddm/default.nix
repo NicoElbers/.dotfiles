dd8f7eaf339763e3d2b0b4caf85e57395fec58f6
Nico Elbers <nico.b.elbers@gmail.com>
2025-09-27 16:15:32 +0200

    Fix sddm theme (rip tokyonight)

--- modules/sddm/default.nix
+++ modules/sddm/default.nix
@@ -1,15 +1,23 @@
 { pkgs, ... }:
 let
-  tokyonight-night-sddm = pkgs.libsForQt5.callPackage ./tokyo-night-sddm.nix { };
+  catppuccin-sddm = pkgs.callPackage ./catppuccin-sddm.nix {
+    flavor = "mocha";
+    accent = "mauve";
+    font  = "Noto Sans";
+    fontSize = "9";
+    background = "${./shacks.png}";
+    loginBackground = true;
+  };
 in
 {
-  environment.systemPackages = [
-    tokyonight-night-sddm
-  ];
+  environment.systemPackages = [(
+    catppuccin-sddm
+  )];
 
   services.displayManager.sddm = {
     enable = true;
-    theme = "tokyo-night-sddm";
+    theme = "catppuccin-mocha-mauve";
+    package = pkgs.kdePackages.sddm;
     wayland.enable = true;
   };
 }
