0ad79d2c73ff456542a49be1c1afc4f124e00eab
Nico Elbers <nico.b.elbers@gmail.com>
2025-10-23 13:31:17 +0200

    fuck up windows :D

--- hosts/omen-laptop/bootloader.nix
+++ hosts/omen-laptop/bootloader.nix
@@ -12,15 +12,16 @@
     enable = true;
     device = "nodev";
     efiSupport = true;
+    useOSProber = true;
 
-    extraEntries = ''
-      menuentry 'Windows Boot Manager (on /dev/nvme0n1p1)' --class windows --class os $menuentry_id_option 'osprober-efi-DC86-0EFE' {
-         insmod part_gpt
-         insmod fat
-         search --no-floppy --fs-uuid --set=root DC86-0EFE
-         chainloader /efi/Microsoft/Boot/bootmgfw.efi
-      }
-    '';
+    # extraEntries = ''
+    #   menuentry 'Windows Boot Manager (on /dev/nvme0n1p1)' --class windows --class os $menuentry_id_option 'osprober-efi-DC86-0EFE' {
+    #      insmod part_gpt
+    #      insmod fat
+    #      search --no-floppy --fs-uuid --set=root DC86-0EFE
+    #      chainloader /efi/Microsoft/Boot/bootmgfw.efi
+    #   }
+    # '';
 
   };
 }
